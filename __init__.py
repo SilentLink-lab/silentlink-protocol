90906520f7de627f177c4248c7b1a0330c32ba71
SilentLink-lab <silentlinkcom@gmail.com>
2024-11-20 04:27:59 +0400

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,13 @@
+# silentlink/__init__.py
+
+"""
+SilentLink - Secure and Private Messaging Protocol
+"""
+
+__version__ = '1.0.0'
+
+from .crypto import *
+from .protocol import Protocol
+from .user import User
+from .server import Server
+from .utils import *
