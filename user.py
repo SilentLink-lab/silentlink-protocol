03179a3e5e17d884eb8139b69784ca647ed06d53
SilentLink <silentlinkcom@gmail.com>
2024-11-21 04:16:10 +0400

    Update user.py

--- user.py
+++ user.py
@@ -1,6 +1,13 @@
 # silentlink/user.py
 
-from .crypto import generate_x25519_keypair, generate_kyber_keypair, generate_dilithium_keypair, serialize_public_key, dilithium_sign_message
+from .crypto import (
+    generate_x25519_keypair,
+    generate_kyber_keypair,
+    generate_dilithium_keypair,
+    serialize_x25519_public_key,
+    serialize_kyber_public_key,
+    dilithium_sign_message
+)
 import uuid
 import json
 import asyncio
@@ -8,6 +15,12 @@ import websockets
 
 class User:
     def __init__(self, username):
+        """
+        Инициализирует пользователя.
+
+        Args:
+            username (str): Имя пользователя.
+        """
         self.username = username
         self.device_id = str(uuid.uuid4())
 
@@ -19,15 +32,24 @@ class User:
         self.ik_pq_private, self.ik_pq_public = generate_kyber_keypair()
         self.spk_pq_private, self.spk_pq_public = generate_kyber_keypair()
 
-        # Подпись SPK
-        # Здесь добавьте код для подписания SPK с помощью Dilithium
-        # self.spk_pq_signature = dilithium_sign_message(self.ik_pq_private, self.spk_pq_public)
+        # Подпись SPK с использованием Dilithium
+        self.dilithium_private_key, self.dilithium_public_key = generate_dilithium_keypair()
+        self.spk_signature = dilithium_sign_message(
+            self.dilithium_private_key,
+            serialize_x25519_public_key(self.spk_classic_public) + self.spk_pq_public
+        )
 
         self.sessions = {}
         self.websocket = None
         self.server_uri = None
 
     async def connect(self, server_uri):
+        """
+        Подключается к серверу и регистрирует пользователя.
+
+        Args:
+            server_uri (str): URI сервера WebSocket.
+        """
         self.server_uri = server_uri
         self.websocket = await websockets.connect(server_uri)
 
@@ -35,17 +57,21 @@ class User:
         registration_data = {
             'username': self.username,
             'public_keys': {
-                'ik_classic_public': serialize_public_key(self.ik_classic_public).hex(),
-                'spk_classic_public': serialize_public_key(self.spk_classic_public).hex(),
+                'ik_classic_public': serialize_x25519_public_key(self.ik_classic_public).hex(),
+                'spk_classic_public': serialize_x25519_public_key(self.spk_classic_public).hex(),
                 'ik_pq_public': self.ik_pq_public.hex(),
                 'spk_pq_public': self.spk_pq_public.hex(),
-                # Добавьте подписи и другие необходимые данные
+                'spk_signature': self.spk_signature.hex()
+                # Добавьте другие необходимые данные
             }
         }
 
         await self.websocket.send(json.dumps(registration_data))
 
     async def listen(self):
+        """
+        Прослушивает входящие сообщения от сервера.
+        """
         async for message in self.websocket:
             data = json.loads(message)
             sender = data['sender']
@@ -55,17 +81,35 @@ class User:
             print(f"Received message from {sender}: {plaintext.decode()}")
 
     async def get_recipient_info(self, recipient_username):
+        """
+        Получает информацию о получателе от сервера.
+
+        Args:
+            recipient_username (str): Имя пользователя получателя.
+
+        Returns:
+            dict: Информация о публичных ключах получателя.
+        """
         # Запрос информации о получателе у сервера
         # В данном примере предполагается, что сервер хранит публичные ключи пользователей
-        async with websockets.connect(self.server_uri) as ws:
-            await ws.send(json.dumps({'action': 'get_user_info', 'username': recipient_username}))
-            response = await ws.recv()
-            data = json.loads(response)
-            if data['status'] == 'success':
-                return data['public_keys']
-            else:
-                return None
+        await self.websocket.send(json.dumps({
+            'action': 'get_user_info',
+            'username': recipient_username
+        }))
+        response = await self.websocket.recv()
+        data = json.loads(response)
+        if data['status'] == 'success':
+            return data['public_keys']
+        else:
+            return None
 
     async def send_message(self, recipient_username, plaintext):
+        """
+        Отправляет сообщение получателю.
+
+        Args:
+            recipient_username (str): Имя пользователя получателя.
+            plaintext (bytes): Сообщение для отправки.
+        """
         protocol = Protocol(self)
         await protocol.send_message(recipient_username, plaintext)
